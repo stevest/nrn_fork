483d7065e6a729b7e1512134f2cbdecfd1cf77c9
Michael Hines <michael.hines@yale.edu>
2009-12-18 07:07:43 -0500

    MulRunFitter YFitness allows specification of the number of equidistant points to be used for fitting of the raw data.

---
+++ src/nrnpython/inithoc.cpp
@@ -0,0 +1,86 @@
+#include "nrnmpiuse.h"
+#ifdef NRNMPI
+#include <mpi.h>
+#endif
+#include <stdio.h>
+#include "nrnmpi.h"
+#include "nrnpython_config.h"
+#include <Python.h>
+#include <stdlib.h>
+
+
+extern "C" {
+
+//int nrn_global_argc;
+extern char** nrn_global_argv;
+
+extern void nrnpy_augment_path();
+extern void nrnpy_hoc();
+
+extern int nrn_is_python_extension;
+extern int ivocmain(int, char**, char**);
+
+#ifdef NRNMPI
+
+static char* argv_mpi[] = {"NEURON", "-mpi","-dll", 0};
+static int argc_mpi = 2;
+
+#endif
+
+static char* argv_nompi[] = {"NEURON", "-dll", 0};
+static int argc_nompi = 1;
+
+static char* env[] = {0};
+void inithoc() {
+	char buf[200];
+	
+	int argc = argc_nompi;
+	char** argv = argv_nompi;
+
+	if (nrn_global_argv) { //ivocmain was already called so already loaded
+		nrnpy_hoc();
+		return;
+	}
+#ifdef NRNMPI
+
+	int flag;
+
+	MPI_Initialized(&flag);
+
+	if (flag) {
+	  printf("MPI_Initialized==true, enabling MPI funtionality.\n");
+
+	  argc = argc_mpi;
+	  argv = argv_mpi;
+	}
+	else {
+	  printf("MPI_Initialized==false, disabling MPI functionality.\n");
+	}
+
+#endif
+	
+
+#if !defined(__CYGWIN__)
+	sprintf(buf, "%s/.libs/libnrnmech.so", NRNHOSTCPU);
+	//printf("buf = |%s|\n", buf);
+	FILE* f;
+	if ((f = fopen(buf, "r")) != 0) {
+		fclose(f);
+		argc += 2;
+		argv[argc-1] = new char[strlen(buf)+1];
+		strcpy(argv[argc-1], buf);
+	}
+#endif
+	nrn_is_python_extension = 1;
+#if NRNMPI
+	nrnmpi_init(1, &argc, &argv); // may change argc and argv
+#endif		
+	ivocmain(argc, argv, env);
+	nrnpy_augment_path();
+	nrnpy_hoc();
+}
+
+#if !defined(CYGWIN)
+void modl_reg() {}
+#endif
+}
