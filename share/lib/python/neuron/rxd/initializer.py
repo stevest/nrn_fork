93b8a442626d2675fe01597f30f477d0e52d4a51
Robert A McDougal <Robert A McDougal@localhost>
2014-10-06 16:47:02 -0400

    rxd: bug fixes for delayed initialization

---
+++ share/lib/python/neuron/rxd/initializer.py
@@ -0,0 +1,27 @@
+import itertools
+
+# TODO: monkey-patch everything that requires an init so only even attempts once
+
+has_initialized = False
+def _do_init():
+    global has_initialized
+    if has_initialized: return
+    from . import species, region, rxd
+    if len(species._all_species) > 0:
+        has_initialized = True
+        for obj in itertools.chain(region._all_regions, species._all_species, rxd._all_reactions):
+            obj = obj()
+            if obj is not None:
+                obj._do_init()
+        rxd._init()
+
+def is_initialized():
+    return has_initialized
+
+def assert_initialized(msg=''):
+    from . import species
+    if not has_initialized and len(species._all_species) > 0:
+        if len(msg):
+            msg = ': ' + msg
+        from .rxdException import RxDException
+        raise RxDException('invalid operation; rxd module not initialized' + msg)
