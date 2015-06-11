119a363b64765081e4391fa264b6b2920e7a00a4
Michael Hines <michael.hines@yale.edu>
2015-06-11 08:24:59 -0400

    bbsavestate branch from a bbsavestate patch manually derived from the bitbucket bbsavestate repository. The difficulty is that the latter also contains many coreneuron changes that are not appropriate to this branch. The manual changes were gathered from the bitbucket version as of d155722 2014-04-14.

---
+++ src/nrniv/nrnpy.cpp
@@ -0,0 +1,243 @@
+#include <../../nrnconf.h>
+// For Linux and Max OS X,
+// Solve the problem of not knowing what version of Python the user has by
+// possibly deferring linking to libnrnpython.so to run time using the proper
+// Python interface
+
+#include <../nrnpython/nrnpython_config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <InterViews/resource.h>
+#include "nrnoc2iv.h"
+#include "classreg.h"
+#include "nonvintblock.h"
+#include "nrnmpi.h"
+
+extern "C" {
+extern int nrn_nopython;
+extern int nrnpy_nositeflag;
+int* nrnpy_site_problem_p;
+extern void (*p_nrnpython_start)(int);
+void nrnpython();
+static void (*p_nrnpython_real)();
+static void (*p_nrnpython_reg_real)();
+}
+
+// following is undefined or else has the value of sys.api_version
+// at time of configure (using the python first in the PATH).
+#if defined(NRNPYTHON_DYNAMICLOAD)
+
+#ifdef MINGW
+#define RTLD_NOW 0
+#define RTLD_GLOBAL 0
+#define RTLD_NOLOAD 0
+extern "C" {
+extern void* dlopen_noerr(const char* name, int mode);
+#define dlopen dlopen_noerr
+extern void* dlsym(void* handle, const char* name);
+extern int dlclose(void* handle);
+extern char* dlerror();
+}
+#else
+//#define _GNU_SOURCE
+#include <dlfcn.h>
+#endif
+
+extern "C" {
+extern char* neuron_home;
+}
+#if NRNPYTHON_DYNAMICLOAD == 1013
+#ifdef MINGW
+static const char* ver[] = {"2.7", 0};
+#else
+static const char* ver[] = {"2.7", "2.6", "2.5", 0};
+#endif
+#define npylib "libnrnpython1013"
+#endif
+#if NRNPYTHON_DYNAMICLOAD == 1012
+static const char* ver[] = {"2.4", "2.3", 0};
+#define npylib "libnrnpython1012"
+#endif
+
+static int iver; // which python is loaded?
+static void* python_already_loaded();
+static void* load_python();
+static void load_nrnpython();
+#else
+extern "C" {
+extern void nrnpython_start(int);
+extern void nrnpython_reg_real();
+extern void nrnpython_real();
+}
+#endif
+
+void nrnpython() {
+#if USE_PYTHON
+	if (p_nrnpython_real) {
+		(*p_nrnpython_real)();
+		return;
+	}
+#endif	
+	hoc_retpushx(0.);
+}
+
+// Stub class for when Python does not exist
+static void* p_cons(Object*) {
+	return 0;
+}
+static void p_destruct(void* v) {
+}
+static Member_func p_members[] = {0,0};
+
+#if NRNPYTHON_DYNAMICLOAD
+static char* pyhome;
+static void siteprob(void) {
+	if (nrnpy_site_problem_p && (*nrnpy_site_problem_p)) {
+printf("Py_Initialize exited. PYTHONHOME probably needs to be set correctly.\n");
+		if(pyhome) {
+printf("Our automatic guess based on the Python shared library location:\n    export PYTHONHOME=%s\ndid not work.\n", pyhome);
+		}
+printf("It will help to examine the output of:\nnrnpyenv.sh\n\
+and set the indicated environment variables, or avoid python by adding\n\
+nopython: on\n\
+to %s/lib/nrn.defaults (or .nrn.defaults in your $HOME directory)\n",
+neuron_home);
+	}
+}
+
+static void set_pythonhome(void* handle){
+	if (nrnmpi_myid == 0) {atexit(siteprob);}
+#ifdef MINGW
+#else
+	if (getenv("PYTHONHOME") || nrnpy_nositeflag) { return; }
+	Dl_info dl_info;
+	void* s = dlsym(handle, "Py_Initialize");
+        assert(s != NULL);
+	int success = dladdr(s, &dl_info);
+	if (success) {
+		//printf("%s\n", dl_info.dli_fname);
+		pyhome = strdup(dl_info.dli_fname);
+		char* p = pyhome;
+		int n = strlen(p);
+		int seen = 0;
+		for (int i = n-1; i > 0; --i) {
+			if (p[i] == '/') {
+				if (++seen >= 2) {
+					p[i] = '\0' ;
+					break;
+				}
+			}
+		}
+		assert(setenv("PYTHONHOME", p, 1) == 0);
+	}
+#endif
+}
+#endif
+
+void nrnpython_reg() {
+	//printf("nrnpython_reg in nrnpy.cpp\n");
+#if USE_PYTHON
+    if (nrn_nopython) {
+	p_nrnpython_start = 0;
+	p_nrnpython_real = 0;
+	p_nrnpython_reg_real = 0;
+    }else{
+#if NRNPYTHON_DYNAMICLOAD
+	void* handle = python_already_loaded();
+	if (!handle) { // embed python
+		handle = load_python();
+	}
+	if (handle) {
+		// need to worry about the site.py problem
+		// can fix with a proper PYTHONHOME but need to know
+		// what path was used to load the python library.
+		set_pythonhome(handle);
+		load_nrnpython();
+	}
+#else
+	p_nrnpython_start = nrnpython_start;
+	p_nrnpython_real = nrnpython_real;
+	p_nrnpython_reg_real = nrnpython_reg_real;
+#endif
+    }
+	if (p_nrnpython_reg_real) {
+		(*p_nrnpython_reg_real)();
+		if (nrnpy_site_problem_p) {
+			*nrnpy_site_problem_p = 1;
+		}
+		return;
+	}
+#endif
+	class2oc("PythonObject", p_cons, p_destruct, p_members, NULL, NULL, NULL);
+}
+
+#if NRNPYTHON_DYNAMICLOAD // to end of file
+
+// important dlopen flags :
+// RTLD_NOLOAD returns NULL if not open, or handle if it is resident.
+
+static void* ver_dlo(int flag) {
+	for (int i = 0; ver[i]; ++i) {
+		char name[100];
+#ifdef MINGW
+		sprintf(name, "python%c%c.dll", ver[i][0], ver[i][2]);
+#else	
+#if DARWIN
+		sprintf(name, "libpython%s.dylib", ver[i]);
+#else
+		sprintf(name, "libpython%s.so", ver[i]);
+#endif
+#endif
+		void* handle = dlopen(name, flag);
+		iver = i;
+		if (handle) {
+			return handle;
+		}
+	}
+	iver = -1;
+	return NULL;
+}
+
+static void* python_already_loaded() {
+	void* handle = ver_dlo(RTLD_NOW|RTLD_GLOBAL|RTLD_NOLOAD);
+	//printf("python_already_loaded %d\n", iver);
+	return handle;
+}
+
+static void* load_python() {
+	void* handle = ver_dlo(RTLD_NOW|RTLD_GLOBAL);
+	//printf("load_python %d\n", iver);
+	return handle;
+}
+
+static void* load_sym(void* handle, const char* name) {
+	void* p = dlsym(handle, name);
+	if (!p) {
+		printf("Could not load %s\n", name);
+		exit(1);
+	}
+	return p;
+}
+
+static void load_nrnpython() {
+	char name[100];
+#ifdef MINGW
+	sprintf(name, "%s.dll", npylib);
+#else
+#if DARWIN
+	sprintf(name, "%s/../../%s/lib/%s.dylib", neuron_home, NRNHOSTCPU, npylib);
+#else
+	sprintf(name, "%s.so", npylib);
+#endif
+#endif
+	void* handle = dlopen(name, RTLD_NOW);
+	if (!handle) {
+		printf("Could not load %s\n", name);
+		return;
+	}
+	p_nrnpython_start = (void(*)(int))load_sym(handle, "nrnpython_start");
+	p_nrnpython_real = (void(*)())load_sym(handle, "nrnpython_real");
+	p_nrnpython_reg_real = (void(*)())load_sym(handle, "nrnpython_reg_real");
+}
+
+#endif
